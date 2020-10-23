595f9233e377a84edab8f87912c92e30bf64b421
Ryohei Endo <r-endo@studio-spoon.co.jp>
2020-10-24 00:55:59 +0900

    Fix web routes

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/**
+ * React アプリケーションを `/api` を除く全てのパスにマッチさせる
+ */
 
-Route::get('{any}', function () {
-    return view('app');
-})->where('any', '.*');
+Route::view('/{any}', 'app')->where('any', '^(?!api).*');
