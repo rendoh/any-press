241bdf65d57c5bc58d3b16327b98708c624e2db8
Ryohei Endo <r-endo@studio-spoon.co.jp>
2020-10-31 01:43:50 +0900

    chore

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 import 'ress';
 import 'react-toastify/dist/ReactToastify.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = () => {
+  const App = require('./components/App').default;
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
 
-if (module.hot) {
-  module.hot.accept();
+render();
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./components/App');
 }
