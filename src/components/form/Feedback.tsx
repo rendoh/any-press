cf968553002536890803cb3d08af85b02b5449c7
Ryohei Endo <r-endo@studio-spoon.co.jp>
2020-11-02 01:30:57 +0900

    コンポーネント追加

---
+++ src/components/form/Feedback.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+import { Colors } from '../../constants/styles';
+
+const Feedback = styled.p`
+  color: ${Colors.error};
+  font-size: 12px;
+  margin-top: 10px;
+`;
+
+export default Feedback;
