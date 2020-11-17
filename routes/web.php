5b7ab179838cae88f417daa07e48d8489237d58c
Ryohei Endo <r-endo@studio-spoon.co.jp>
2020-11-17 19:58:52 +0900

    Fix routes

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleDetailController;
 
+Route::get('/articles/{article}', ArticleDetailController::class)
+    ->where('article', '[0-9]+');
+
 /**
  * React アプリケーションを `/api` を除く全てのパスにマッチさせる
  */
-
-Route::get('/articles/{article}', ArticleDetailController::class);
-
 Route::view('/{any}', 'app', [
     'title' => config('app.name'),
     'description' => 'AnyPress',
